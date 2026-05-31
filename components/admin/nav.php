8472f3afd8d54a284285c46fd1711201e5b0bd03
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-05-31 06:46:12 -0700

    feat(nav): enhance navLayout function to display website logo

--- components/admin/nav.php
+++ components/admin/nav.php
@@ -1,6 +1,6 @@
 <?php
 
-function navLayout($name, $path = "")
+function navLayout($name, $path = "", $dark = false)
 {
     $items = array(
         [
@@ -47,20 +47,16 @@ function navLayout($name, $path = "")
         </li>';
     }
 
-    return '
-    <div class="flex flex-col h-full space-y-2">
-        <div class="mb-6 px-2">
-            <span class="text-2xl font-black tracking-wider text-indigo-600 font-[Anton]">JAMIS ART</span>
-        </div>
-        <ul class="flex flex-col space-y-1 flex-1">
+    return " <div class='flex flex-col h-full space-y-2'>
+       <img src='$path/" . ($dark ? 'assets/img/jemis-art-dark.png' : 'assets/img/jemis-art.png') . "' alt='Logo' class='w-32 h-auto mx-auto'>
+        <ul class='flex flex-col space-y-1 flex-1'>
             ' . $navItems . '
         </ul>
-        <div class="mt-auto pt-4 border-t border-gray-200">
-             <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2 text-red-600 transition hover:bg-red-50">
-                <svg class="w-5 h-5 opacity-75" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
-                <span class="font-medium text-sm">Sign Out</span>
+        <div class='mt-auto pt-4 border-t border-gray-200'>
+             <a href='#' class='flex items-center gap-3 rounded-lg px-3 py-2 text-red-600 transition hover:bg-red-50'>
+                <svg class='w-5 h-5 opacity-75' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1'></path></svg>
+                <span class='font-medium text-sm'>Sign Out</span>
             </a>
         </div>
-    </div>
-    ';
+    </div>";
 }
